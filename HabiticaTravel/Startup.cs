1176c935ff35dfb4219c6b8ccf645fd737bf9160
James J <jjackson360@gmail.com>
2018-03-05 10:29:59 -0500

    first commit

---
+++ HabiticaTravel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HabiticaTravel.Startup))]
+namespace HabiticaTravel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
