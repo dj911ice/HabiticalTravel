097352401f3b7736a58467788954bee4b168e011
Justin Dickerson <justin.p.dickerson@gmail.com>
2018-03-05 16:34:32 -0500

    Created: Habitica Login view and form

---
+++ HabiticaTravel/Controllers/Habitica/HabiticaAccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HabiticaTravel.Controllers.Habitica
+{
+    public partial class HabiticaController : Controller
+    {
+        // GET: HabiticaAccount
+
+        public ActionResult HabiticaLogin()
+        {
+            return View();
+        }
+        public ActionResult HabiticaAccountCredentials()
+            {
+            return View();
+        }
+    }
+}
