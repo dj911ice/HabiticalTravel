97f6934e411f842a1d8dfa2352193c0585369c37
James J <jjackson360@gmail.com>
2018-03-05 13:25:35 -0500

    added some controller information for a register post

---
+++ HabiticaTravel/Controllers/HabiticaController.cs
@@ -0,0 +1,55 @@
+﻿using HabiticaTravel.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HabiticaTravel.Controllers
+{
+    public partial class HabiticaController : Controller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public ActionResult PostRegisterUser(ApplicationUser user, RegisterViewModel model)
+        {
+            // 42.335722   lon=-83.049944 
+            string uri = "https://habitica.com/api/v3/user";
+            var request = WebRequest.Create(uri);
+            if (request != null)
+            {
+                request.Method = "POST";
+                request.Timeout = 20000;
+                request.ContentType = "application/json";
+                string json = $"";
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+
+                    streamWriter.Write(json);
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                using (Stream response = request.GetResponse().GetResponseStream())
+                {
+                    using (StreamReader sr = new StreamReader(response))
+                    {
+                        var output = sr.ReadToEnd();
+                        JObject JSON = JObject.Parse(output);
+                        ViewBag.Data = JSON["data"];
+                        return View();
+                    }
+
+                }
+                // request.GetRequestStream().Close();  
+            }
+            return View("./Shared/Error");
+
+        }
+    }
+}
