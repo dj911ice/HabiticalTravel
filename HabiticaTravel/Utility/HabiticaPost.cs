ec41c0b5300433a2dea7ca5ed340bf30b0bd67f0
dani.dearmond <dani.dearmond@gmail.com>
2018-03-05 16:24:15 -0500

    Fixed partial classes for HabiticaPost

--- HabiticaTravel/Utility/HabiticaPost.cs
+++ HabiticaTravel/Utility/HabiticaPost.cs
@@ -25,16 +25,5 @@ namespace HabiticaTravel.Utility
         }
     }
 
-    public static partial class HabiticaPost
-    {
-        public static async void ResetPassword(ApplicationUser user, ResetPasswordViewModel model)
-        {
-            var ResponseString = await "https://habitica.com/api/v3/user/reset-password"
-                .PostUrlEncodedAsync(new
-                {
-                    email = user.Email,
-                })
-                .ReceiveString();
-        }
-    }
+
 }
---
+++ HabiticaTravel/Utility/HabiticaResetPassword.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HabiticaTravel.Utility
+{
+    public static partial class HabiticaPost
+    {
+        public static async void ResetPassword(ApplicationUser user, ResetPasswordViewModel model)
+        {
+            var ResponseString = await "https://habitica.com/api/v3/user/reset-password"
+                .PostUrlEncodedAsync(new
+                {
+                    email = user.Email,
+                })
+                .ReceiveString();
+        }
+    }
+}
