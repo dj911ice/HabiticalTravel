5fdb18586f8a808e19ee298c0dd792591ba59da9
dani.dearmond <dani.dearmond@gmail.com>
2018-03-06 11:26:29 -0500

    Added empty Controller and View for Default Tasks.

---
+++ HabiticaTravel/Controllers/DefaultTasksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HabiticaTravel.Models;
+
+namespace HabiticaTravel.Controllers
+{
+    public class DefaultTasksController : Controller
+    {
+        // GET: DefaultTasks
+        public ActionResult ShowDefaultTasks()
+        {
+            habiticatravelEntities MyHabitica = new habiticatravelEntities();
+            
+
+            return View();
+        }
+    }
+}
