f9e402b63b7950b977459c0554ce5423c427e396
James J <jjackson360@gmail.com>
2018-03-07 11:23:33 -0500

    edited index page and added css

--- HabiticaTravel/App_Start/BundleConfig.cs
+++ HabiticaTravel/App_Start/BundleConfig.cs
@@ -30,6 +30,9 @@ namespace HabiticaTravel
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Habitica").Include(
+                       "~/Content/HomeLogin.css"));
         }
     }
 }
