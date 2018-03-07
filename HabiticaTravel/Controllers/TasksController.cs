4695ae64802e8bec5005460d3404e87891f2a069
dani.dearmond <dani.dearmond@gmail.com>
2018-03-07 14:20:06 -0500

    Clone Task functionality added to TaskController

--- HabiticaTravel/Controllers/TasksController.cs
+++ HabiticaTravel/Controllers/TasksController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using HabiticaTravel.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
 
 namespace HabiticaTravel.Controllers
 {
@@ -21,8 +25,50 @@ namespace HabiticaTravel.Controllers
             return View();
         }
 
-        public ActionResult CloneTask()
+        public ActionResult CloneTask(int TaskId)
         {
+            habiticatravelEntities MyHabitica = new habiticatravelEntities();
+            DefaultTask TaskToClone = MyHabitica.DefaultTasks.Find(TaskId);
+            ApplicationUser MyUser = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            CustomTask ClonedTask = new CustomTask
+            {
+                TaskName = TaskToClone.TaskName,
+                TaskType = TaskToClone.TaskType,
+                TaskTag = TaskToClone.TaskTag,
+                TaskNotes = TaskToClone.TaskNotes,
+                TaskDueDate = TaskToClone.TaskDueDate,
+                TaskDifficulty = TaskToClone.TaskDifficulty,
+                ReminderId = TaskToClone.ReminderId,
+                ReminderStartDate = TaskToClone.ReminderStartDate,
+                ReminderTime = TaskToClone.ReminderTime,
+                TaskFrequency = TaskToClone.TaskFrequency,
+                TaskRepeat = TaskToClone.TaskRepeat,
+                TaskStreak = TaskToClone.TaskStreak,
+                TaskStartDate = TaskToClone.TaskStartDate,
+                TaskHabitUp = TaskToClone.TaskHabitUp,
+                TaskHabitDown = TaskToClone.TaskHabitDown,
+                TaskRewardValue = TaskToClone.TaskRewardValue,
+                EveryXDays = TaskToClone.EveryXDays,
+                UserId = MyUser.Id
+            };
+
+            MyHabitica.CustomTasks.Add(ClonedTask);
+            MyHabitica.SaveChanges();
+
+            List<DefaultTaskItem> CloneItemsList = new List<DefaultTaskItem>(MyHabitica.DefaultTasks.Find(TaskId).DefaultTaskItems.ToList());
+            List<CustomTaskItem> NewItemsList = new List<CustomTaskItem>();
+            
+            for (int i = 0; i < CloneItemsList.Count; i++)
+            {
+                CustomTaskItem TempItem = new CustomTaskItem
+                {
+                    ItemName = CloneItemsList[i].ItemName,
+                    TaskId = MyHabitica.CustomTasks.Find(ClonedTask).TaskId
+                };
+                MyHabitica.CustomTaskItems.Add(TempItem);
+            }
+            MyHabitica.SaveChanges();
+
             return View();
         }
     }
