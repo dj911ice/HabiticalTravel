d31e0ded73b63f85cec429db8bece7455dfd0369
James J <jjackson360@gmail.com>
2018-03-08 10:19:46 -0500

    added custom class for Task And Item

---
+++ HabiticaTravel/Models/CustomModels/TaskAndItems.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HabiticaTravel.Models
+{
+    public partial class TaskAndItem
+    {
+        public CustomTask CustomTask { get; set; }
+        public CustomTaskItem CustomTaskItem { get; set; }
+    }
+}
