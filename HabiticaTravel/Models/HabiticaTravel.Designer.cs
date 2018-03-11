520155cffcb30fb5077e95e106ae7c732ed25425
dani.dearmond <dani.dearmond@gmail.com>
2018-03-11 12:23:57 -0400

    Trying the entity model update again to see what sticks.

--- HabiticaTravel/Models/HabiticaTravel.Designer.cs
+++ HabiticaTravel/Models/HabiticaTravel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Dani\source\repos\HabiticalTravel\HabiticaTravel\Models\HabiticaTravel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Dani\Source\Repos\HabiticalTravel\HabiticaTravel\Models\HabiticaTravel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
