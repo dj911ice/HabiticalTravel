b1dc0684be4fb51bb309acf9152451f3f2bdb153
Hanio90 <hanio90@gmail.com>
2018-03-11 12:14:18 -0400

    added grp controller/view

---
+++ HabiticaTravel/Controllers/GroupController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HabiticaTravel.Controllers
+{
+    public class GroupController : Controller
+    {
+        // GET: Group
+        public ActionResult DisplayGroup() // Essentially our index View
+        {
+            return View();
+        }
+    }
+}
